11299025471ffc187b99a7fb28c9d8f043ec54d6
Pulkit Agarwal <pulkitgarg2493@gmail.com>
2023-08-23 10:16:49 +0530

    chore: singly link list remain

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -1,6 +1,3 @@
-
-console.log("Hello Wworlkd");
-
 class Node{
 constructor(val){
     this.val = val;
@@ -26,11 +23,14 @@ class LinkedList{
         this.tail = node;
         }
         this.length++;
+        return this;
     }
 
     pop(){
         if(this.length===0)return;
+        let removed;
         if(this.length === 1){
+            removed = this.head;
             this.head =  null;
             this.tail = null;
             this.length = 0;
@@ -39,10 +39,12 @@ class LinkedList{
             while(curr.next !== this.tail){
                 curr = curr.next;
             }
+            removed = curr.next;
             this.tail = curr;
             this.tail.next = null;
             this.length--;
         }
+        return removed;
     }
 
     shift(){
@@ -56,6 +58,92 @@ class LinkedList{
         }
         this.length--;
         currentNode.next = null;
+        return currentNode;
+    }
+
+    unshift(val){
+        var newHead = new Node(val);
+        newHead.next = this.head;
+        this.head = newHead;
+        this.length++;
+        if(!this.tail){
+            this.tail = this.head;
+        }
+        return newHead;
+    }
+
+    get(ind){
+        if(ind>=this.length || ind<0)return;
+
+        let i=0,matchNode=this.head;
+        while(i!=ind){
+            matchNode=matchNode.next;
+            i++;
+        }
+        return matchNode;
+    }
+
+    set(ind,val){
+        var node = this.get(ind);
+        if(node){
+            node.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(ind,val){
+        if(ind>this.length || ind<0){
+            return false;
+        }
+        if(ind===0){
+            return !!this.unshift(val);
+        }
+        if(ind === this.length){
+            return !!this.push(val);
+        }
+        var node = new Node(val);
+            var prevNode = this.get(ind-1);
+            node.next =  prevNode.next;
+            prevNode.next=node;
+        this.length++;
+        return true;
+    }
+
+    remove(ind){
+        if(ind<0 || ind>=this.length)return undefined;
+
+        if(ind===this.length-1){
+            return this.pop();
+        }
+
+        if(ind ===0){
+            return this.shift();
+        }
+
+        let prevNode = this.get(ind-1);
+        let removed = prevNode.next; 
+        prevNode.next = removed.next;
+        removed.next = null;
+        return removed; 
+    }
+
+    reverse(){
+        let curr = this.head,next;
+        if(curr){
+            next=curr.next;
+            this.head.next = null;
+        }    
+        while(curr && next){
+            var newNext = next.next;
+            next.next= curr;
+            curr = next;
+            next = newNext;
+        }
+
+        this.tail = this.head;
+        this.head=curr;
+        return this;
     }
 
 }
@@ -73,13 +161,78 @@ var linkList = new LinkedList();
 // console.log(linkList);
 
 
+// linkList.push("1");
+// linkList.push("2");
+// linkList.push("3");
+// console.log(linkList);
+// linkList.shift();
+// linkList.shift();
+// linkList.shift();
+// console.log(linkList);
+
+
+
+// console.log(linkList);
+// linkList.unshift(1);
+// linkList.unshift(2);
+// linkList.unshift(3);
+// console.log(linkList);
+// linkList.pop();
+// linkList.pop();
+// linkList.pop();
+// linkList.unshift(3);
+// console.log(linkList);
+
+
+// linkList.push("1");
+// linkList.push("2");
+// linkList.push("3");
+// console.log(linkList);
+// console.log(linkList.get(1));
+// console.log(linkList.get(3));
+// console.log(linkList.get(0));
+
+// linkList.push("1");
+// linkList.push("2");
+// linkList.push("3");
+// console.log(linkList);
+// console.log(linkList.set(1,"Hello"));
+// console.log(linkList.set(3,"-1"));
+// console.log(linkList.set(0,"3"));
+
+// console.log(linkList);
+
+
+// linkList.push("1");
+// console.log(linkList);
+// console.log(linkList.insert(1,"Hello"));
+// console.log(linkList);
+// console.log(linkList.insert(4,"-1"));
+// console.log(linkList);
+// console.log(linkList.insert(0,"3"));
+// console.log(linkList);
+
+
+// linkList.push("1");
+// linkList.push("2");
+// linkList.push("3");
+// console.log(linkList);
+// linkList.remove(2);
+// console.log(linkList);
+// console.log(linkList.remove(2));
+// console.log(linkList.remove(0));
+// console.log(linkList);
+
+
 linkList.push("1");
 linkList.push("2");
 linkList.push("3");
+linkList.reverse();
 console.log(linkList);
-linkList.shift();
-linkList.shift();
-linkList.shift();
+linkList.reverse();
 console.log(linkList);
-
-
+console.log(linkList.pop());
+console.log(linkList.pop());
+console.log(linkList.pop());
+console.log(linkList);
+console.log(linkList.reverse());
